3a6021ea215658f9f071de7bfcefb3d79e033c8f
Lufei Deng <denglufei@gmail.com>
2022-07-06 11:22:30 -0400

    Initialize the backend service project

---
+++ smsbackend/src/main/java/com/starphysics/smsbackend/SmsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.starphysics.smsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsbackendApplication.class, args);
+	}
+
+}
---
+++ smsbackend/src/test/java/com/starphysics/smsbackend/SmsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starphysics.smsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
